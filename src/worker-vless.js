70517b701ed022aba34144792b34a0c986ebe5d9
longyan2009 <77277579+longyan2009@users.noreply.github.com>
2023-08-07 22:20:43 +0800

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '7e33ad74-551a-48c6-a39c-3867d230188b';
 
-let proxyIP = '';
+let proxyIP = 'cdn.anycast.eu.org';
 
 
 if (!isValidUUID(userID)) {
